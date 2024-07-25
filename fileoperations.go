6f37919ba68691a9f76642f35cf5dc902c10bb7c
Amol Deodhar <amold1@gmail.com>
2024-07-25 09:34:16 -0400

    add fileio operations

---
+++ fileoperations.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+type Cluster struct {
+	Name   string   `json:"name"`
+	IPList []string `json:"ipList"`
+}
+
+var allClusters []Cluster
+
+func fileIO(data Cluster) {
+	byteValue, _ := os.ReadFile("log.json")
+	if len(byteValue) != 0 {
+		if err := json.Unmarshal(byteValue, &allClusters); err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+	foundData := false
+	for index, cluster := range allClusters {
+		if cluster.Name == data.Name {
+			foundData = true
+			allClusters[index] = data
+		}
+	}
+	if !foundData {
+		allClusters = append(allClusters, data)
+	}
+	dataToWrite, err := json.Marshal(allClusters)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = os.WriteFile("log.json", dataToWrite, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
--- main.go
+++ main.go
@@ -1,14 +1,23 @@
 package main
 
-import "log"
-
 func main() {
 	// v8GetRecord()
-	if err := setRecord("test.akafn.com", "A", "1.1.1.4"); err != nil {
-		log.Fatal(err)
-	}
-	// getRecord("test.akafn.com")
+	// if err := setRecord("test.akafn.com", "A", "1.1.1.4"); err != nil {
+	// 	log.Fatal(err)
+	// }
+	// result, err := getRecord("dodo.akafn.com", "A")
+	// if strings.Contains(err.Error(), "sundar") {
+	// 	fmt.Println("Jai Gopal")
+	// } else {
+	// 	fmt.Println(result)
+	// }
 	// GetKey()
 	// getAllRecordSets()
 	// interactWithLinodeGo()
+	var data = Cluster{
+		Name:   "abir",
+		IPList: []string{"ip1", "ip2", "ip3"},
+	}
+
+	fileIO(data)
 }
